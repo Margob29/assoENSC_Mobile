01fbba2a9a95f62be017618897c517c3c7339148
Margob29 <margobiret@yahoo.fr>
2023-03-24 16:01:26 +0100

    event navigation

---
+++ navigation/EventStackNavigator.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import EventDetails from "../screen/Events/EventDetails";
+import EventScreen from "../screen/Events/EventScreen";
+
+const EventsStack = createNativeStackNavigator();
+
+export default function EventsStackNavigator() {
+  return (
+    <EventsStack.Navigator initialRouteName="EventsList">
+      <EventsStack.Screen
+        name="EventsList"
+        component={EventScreen}
+        options={{ title: "Evènements" }}
+      />
+      <EventsStack.Screen name="EventDetails" component={EventDetails} />
+    </EventsStack.Navigator>
+  );
+}
--- screen/Events/EventScreen.js
+++ screen/Events/EventScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
-import styles from "../theme/styles";
+import styles from "../../theme/styles";
 //import eventService from "../api/EventService";
 
 export default function EventScreen() {
